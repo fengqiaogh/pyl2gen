40ee033ed95551a2108c3b533f1d081c36f08be1
fengqiaogh <qiaofeng@sjtu.edu.cn>
2025-01-06 20:01:55 +0800

    feat: enhance gaseous transmittance functions with ancillary data integration and improve Rayleigh class structure

--- gas_trans.py
+++ gas_trans.py
@@ -1,7 +1,7 @@
-import re
+import numpy as np
 from l12_parms import O3_BIT, CO2_BIT, NO2_BIT, H2O_BIT
 
-import numpy as np
+from ancillary import Ancillary
 
 
 def ozone_transmittance(oz, k_oz, csolz, csenz, tg_sol, tg_sen, tg):
@@ -16,7 +16,9 @@ def co2_transmittance():
     pass
 
 
-def no2_transmittance():
+def no2_transmittance(
+    no2_strat, no2_tropo, no2_frac, k_no2, csolz, csenz, tg_sol, tg_sen, tg
+):
     sec0 = 1.0 / csolz
     sec = 1.0 / csenz
     no2_tr200 = no2_frac * no2_tropo
@@ -29,7 +31,21 @@ def no2_transmittance():
     return tg_sol, tg_sen, tg
 
 
-def h2o_transmittance():
+def h2o_transmittance(
+    wv,
+    a_h2o,
+    b_h2o,
+    c_h2o,
+    d_h2o,
+    e_h2o,
+    f_h2o,
+    g_h2o,
+    csolz,
+    csenz,
+    tg_sol,
+    tg_sen,
+    tg,
+):
     t_h2o = a_h2o + wv * (
         b_h2o + wv * (c_h2o + wv * (d_h2o + wv * (e_h2o + wv * (f_h2o + wv * g_h2o))))
     )
@@ -39,14 +55,48 @@ def h2o_transmittance():
     return tg_sol, tg_sen, tg
 
 
-def gaseous_transmittance(settings: dict):
+def gaseous_transmittance(
+    settings: dict, ancillary: Ancillary, sensorinfo, csolz, csenz, tg_sol, tg_sen, tg
+):
+
     if settings["gas_opt"] & O3_BIT:
-        tg_sol, tg_sen, tg = ozone_transmittance()
+        oz = ancillary.oz[..., np.newaxis]
+        tg_sol, tg_sen, tg = ozone_transmittance(
+            oz, sensorinfo.k_oz, csolz, csenz, tg_sol, tg_sen, tg
+        )
     # if settings["gas_opt"] & O3_BIT:
     # tg_sol, tg_sen, tg = co2_transmittance()
     if settings["gas_opt"] & CO2_BIT:
-        tg_sol, tg_sen, tg = no2_transmittance()
+        no2_strat = ancillary.no2_strat[..., np.newaxis]
+        no2_tropo = ancillary.no2_tropo[..., np.newaxis]
+        no2_frac = ancillary.no2_frac[..., np.newaxis]
+        tg_sol, tg_sen, tg = no2_transmittance(
+            no2_strat,
+            no2_tropo,
+            no2_frac,
+            sensorinfo.k_no2,
+            csolz,
+            csenz,
+            tg_sol,
+            tg_sen,
+            tg,
+        )
     if settings["gas_opt"] & H2O_BIT:
-        tg_sol, tg_sen, tg = h2o_transmittance()
+        wv = ancillary.wv[..., np.newaxis]
+        tg_sol, tg_sen, tg = h2o_transmittance(
+            wv,
+            sensorinfo.a_h2o,
+            sensorinfo.b_h2o,
+            sensorinfo.c_h2o,
+            sensorinfo.d_h2o,
+            sensorinfo.e_h2o,
+            sensorinfo.f_h2o,
+            sensorinfo.g_h2o,
+            csolz,
+            csenz,
+            tg_sol,
+            tg_sen,
+            tg,
+        )
 
     return tg_sol, tg_sen, tg
--- rayleigh.py
+++ rayleigh.py
@@ -16,47 +16,50 @@ class Rayleigh:
         taur,
         wave: np.ndarray,
         Fo: np.ndarray,
-        csolz: np.ndarray,
-        csenz: np.ndarray,
+        solz: np.ndarray,
+        senz: np.ndarray,
         delphi: np.ndarray,
+        airmass: np.ndarray,
         ws: np.ndarray,
         pr: np.ndarray,
-        settings: dict,
+        OCDATAROOT,
     ):
 
-        self.Lr = np.zeros((ws.size[0], ws.size[1], wave.size))
-        self.L_q = np.zeros((ws.size[0], ws.size[1], wave.size))
-        self.L_u = np.zeros((ws.size[0], ws.size[1], wave.size))
+        self.Lr = np.zeros((ws.shape[0], ws.shape[1], wave.size))
+        self.L_q = np.zeros((ws.shape[0], ws.shape[1], wave.size))
+        self.L_u = np.zeros((ws.shape[0], ws.shape[1], wave.size))
 
         sigma_m = 0.0731 * np.sqrt(ws)
-        airmass = 1.0 / csolz + 1.0 / csenz
+
         fac = ray_press_wang(taur, airmass, pr)
+
         for idx, wa in enumerate(wave):
 
-            OCDATAROOT = settings["OCDATAROOT"]
             file = OCDATAROOT.joinpath(
                 f"{sensor}", "rayleigh", f"rayleigh_{sensor}_{wa}_iqu.h5"
             )
 
             print(f"Loading Rayleigh LUT {file}")
             with h5py.File(file) as f:
-                senz_tab = f.select("senz")[()]
-                solz_tab = f.select("solz")[()]
-                sigma_tab = f.select("sigma")[()]
+                senz_tab = f["senz"][()]
+                solz_tab = f["solz"][()]
+                sigma_tab = f["sigma"][()]
 
                 # I component
-                i_ray_tab = f.select("i_ray")[()]
+                i_ray_tab = f["i_ray"][()]
 
             tmp = np.zeros((NWIND, NSOL, NSEN))
+            ray_i = np.sum(
+                [
+                    RegularGridInterpolator(
+                        (sigma_tab, solz_tab, senz_tab), i_ray_tab[:, :, m, :]
+                    )(np.stack([sigma_m, solz, senz], axis=-1))
+                    * np.cos(np.deg2rad(delphi) * m)
+                    for m in range(NORDER)
+                ],
+                axis=0,
+            )
 
-            for m in range(0, NORDER):
-                tmp[:, :, :] = i_ray_tab[:, :, m, :]
-                interpolater = RegularGridInterpolator(
-                    (sigma_tab, solz_tab, senz_tab), tmp
-                )
-                target_points = np.array([sigma_m, csolz, csenz])
-                interpolated_values = interpolater(target_points.T)
-                ray_i = ray_i + interpolated_values * np.cos(np.deg2rad(delphi) * m)
             self.Lr[:, :, idx] = Fo[idx] * ray_i * fac[:, :, idx]
 
 
