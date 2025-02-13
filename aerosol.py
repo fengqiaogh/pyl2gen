62829c6db1fb216618795281c3ff8e638721be35
fengqiaogh <qiaofeng@sjtu.edu.cn>
2025-02-13 11:15:47 +0800

    feat: refactor Aerosol class to use instance variables and remove Geom class

--- aerosol.py
+++ aerosol.py
@@ -10,18 +10,6 @@ from l12_parms import STDPR
 p0 = STDPR
 
 
-class Geom:
-    def __init__(self, solz, senz, phi):
-        self.senz = senz
-        self.solz = solz
-        self.phi = phi
-        self.csolz = np.cos(np.radians(self.solz))
-        self.csenz = np.cos(np.radians(self.senz))
-        self.airmass = 1 / self.csolz + 1 / self.csenz
-        self.airmass_plp = None
-        self.airmass_sph = None
-
-
 class AerMod:
     def __init__(
         self,
@@ -81,49 +69,49 @@ class AerModTabStr:
     def __init__(self, nmodel, sensorID):
         self.nmodel = nmodel
         self.sensorID = sensorID
-
-        self.wave = None
-        self.solz = None
-        self.senz = None
-        self.phi = None
-        self.scatt = None
-        self.dtran_wave = None
-        self.dtran_theta = None
-        self.dtran_airmass = None
-
         self.model = []
 
 
 class Aerosol:
     nsd = 10
     rhtab = []
+    aer_iter_min = 1
 
-    @classmethod
-    def load_aermod(
-        cls,
+    def __init__(
+        self,
         sensorID: np.int32,
         wave: np.ndarray,
-        nwave: np.int32,
         aermodfile: str,
         models: str,
         nmodels: np.int32,
+        aer_opt: np.int32,
+        iwnir_s_in: np.int32,
+        iwnir_l_in: np.int32,
+        aer_iter_max: np.int32,
+        rhoamin: np.float32,
     ):
-        cls.nmodels = nmodels
+        self.sensorID = sensorID
+        self.wave = wave
+        self.aer_iter_max = aer_iter_max
+        self.nmodels = nmodels
+        self.nir_s = iwnir_s_in
+        self.nir_l = iwnir_l_in
+        self.rhoamin = rhoamin
         print(f"Loading aersol models from {aermodfile}")
 
-        cls.aertab = AerModTabStr(nmodels, sensorID)
+        self.aertab = AerModTabStr(nmodels, sensorID)
 
         for im in range(nmodels):
             file = os.path.join(f"{aermodfile}_{models[im]}.h5")
 
             with h5py.File(file) as f:
-                cls.aertab.wave = f["wave"][:]
-                cls.aertab.solz = f["solz"][:]
-                cls.aertab.senz = f["senz"][:]
-                cls.aertab.phi = f["phi"][:]
-                cls.aertab.scatt = f["scatt"][:]
-                cls.aertab.dtran_wave = f["dtran_wave"][:]
-                cls.aertab.dtran_theta = f["dtran_theta"][:]
+                self.aertab.wave = f["wave"][:]
+                self.aertab.solz = f["solz"][:]
+                self.aertab.senz = f["senz"][:]
+                self.aertab.phi = f["phi"][:]
+                self.aertab.scatt = f["scatt"][:]
+                self.aertab.dtran_wave = f["dtran_wave"][:]
+                self.aertab.dtran_theta = f["dtran_theta"][:]
 
                 rh = f.attrs["Relative Humidity"]
                 sd = f.attrs["Size Distribution"]
@@ -148,28 +136,67 @@ class Aerosol:
                     dtran_a0=f["dtran_a0"][:],
                     dtran_b0=f["dtran_b0"][:],
                 )
-                cls.rhtab.append(rh)
+            self.rhtab.append(rh)
 
-            cls.aertab.model.append(aermod)
-        cls.rhtab = list(set(cls.rhtab))
-        cls.rhtab.sort()
-        cls.nrh = len(cls.rhtab)
-        print(f"Number of Wavelengths                          {cls.aertab.wave.size}")
-        print(f"Number of Solar Zenith Angles                  {cls.aertab.solz.size}")
-        print(f"Number of View Zenith Angles                   {cls.aertab.senz.size}")
-        print(f"Number of Relative Azimuth Angles              {cls.aertab.phi.size}")
-        print(f"Number of Scattering Angles                    {cls.aertab.scatt.size}")
+            self.aertab.model.append(aermod)
+        have_rh = 1
+        self.rhtab = list(set(self.rhtab))
+        self.rhtab.sort()
+        self.nrh = len(self.rhtab)
+        print(f"Number of Wavelengths                          {self.aertab.wave.size}")
+        print(f"Number of Solar Zenith Angles                  {self.aertab.solz.size}")
+        print(f"Number of View Zenith Angles                   {self.aertab.senz.size}")
+        print(f"Number of Relative Azimuth Angles              {self.aertab.phi.size}")
         print(
-            f"Number of Diffuse Transmittance Wavelengths    {cls.aertab.dtran_wave.size}"
+            f"Number of Scattering Angles                    {self.aertab.scatt.size}"
         )
         print(
-            f"Number of Diffuse Transmittance Zenith Angles  {cls.aertab.dtran_theta.size}"
+            f"Number of Diffuse Transmittance Wavelengths    {self.aertab.dtran_wave.size}"
         )
-        cls.aertab.dtran_airmass = 1 / np.cos(np.radians(cls.aertab.dtran_theta))
+        print(
+            f"Number of Diffuse Transmittance Zenith Angles  {self.aertab.dtran_theta.size}"
+        )
+        self.aertab.dtran_airmass = 1 / np.cos(np.radians(self.aertab.dtran_theta))
+        if have_rh and self.aertab.nmodel >= 30:
+            print("Limiting aerosol models based on RH.")
+            use_rh = 1
+        match aer_opt:
+            case -17:
+                print("Using multi-scattering aerosol model selection.")
+                print(f"  and NIR correction with up to {self.aer_iter_max} iterations")
+                print(
+                    f"Using bands at {self.wave[self.nir_s]:4.1f} and {self.wave[self.nir_l]:4.1f} nm for model selection"
+                )
+                print(f"Extrapolating from {self.wave[self.nir_l]:4.1f} nm")
 
-    def ahmader(
-        self, sensorID, wave, nwave, iwnir_s, iwnir_l, nmodels, mindx, geom, wv, rhoa
-    ):
+    def get(self, Fo, La1, csolz, csenz, wv, rh, pr, taur, solz, senz, phi):
+        radref = np.pi / Fo / csolz
+
+        # convert input aerosol radiances to relectance
+        rhoa = La1 * radref
+
+        # require sufficient signal in two NIR bands
+        if (rhoa[-2:] > self.rhoamin).all():
+            # require MS epsilon to be reasonable
+            eps_tmp = rhoa[self.nir_s] / rhoa[self.nir_l]
+            if eps_tmp > 0.1 and eps_tmp < 10:
+                (
+                    rhoa,
+                    modmin1,
+                    modmax1,
+                    modrat1,
+                    modmin2,
+                    modmax2,
+                    modrat2,
+                    eps,
+                    taua,
+                    t_sol,
+                    t_sen,
+                ) = self.rhaer(wv, rh, pr, taur, rhoa, solz, senz, phi)
+        La2 = rhoa / radref
+        return La2, t_sol, t_sen, eps, taua
+
+    def ahmader(self, nmodels, mindx, wv, rhoa, solz, senz, phi):
         (
             modmin,
             modmax,
@@ -181,23 +208,11 @@ class Aerosol:
             rho_pred_min,
             tau_aer,
             rho_aer,
-        ) = self.ahmad_atm_corr(
-            sensorID, wave, nwave, iwnir_s, iwnir_l, nmodels, mindx, geom, wv, rhoa
-        )
+        ) = self.ahmad_atm_corr(nmodels, mindx, wv, rhoa, solz, senz, phi)
         rhoa = rho_aer
         return rhoa, modmin, modmax, modrat, epsnir, tau_pred_min, tau_pred_max
 
-    def rhaer(
-        self, sensorID, wave, nwave, iwnir_s, iwnir_l, geom, wv, rh, pr, taur, rhoa
-    ):
-        # eps1 = 1.0
-        # eps2 = 1.0
-
-        # nmodels = self.nsd
-
-        # taua = -np.ones(nwave)
-        # tsol = -np.ones(nwave)
-        # tsen = -np.ones(nwave)
+    def rhaer(self, wv, rh, pr, taur, rhoa, solz, senz, phi):
         rhoa1 = rhoa
         rhoa2 = rhoa
         # rhoa = BAD_FLT
@@ -222,16 +237,9 @@ class Aerosol:
             eps1,
             tau_pred_min1,
             tau_pred_max1,
-        ) = self.ahmader(
-            sensorID, wave, nwave, iwnir_s, iwnir_l, self.nsd, mindx1, geom, wv, rhoa1
-        )
+        ) = self.ahmader(self.nsd, mindx1, wv, rhoa1, solz, senz, phi)
 
         taua1, tsol1, tsen1 = self.diff_tran(
-            sensorID,
-            wave,
-            nwave,
-            iwnir_l,
-            geom,
             wv,
             pr,
             taur,
@@ -241,6 +249,9 @@ class Aerosol:
             rhoa1,
             tau_pred_min1,
             tau_pred_max1,
+            solz,
+            senz,
+            phi,
         )
 
         if irh2 != irh1:
@@ -253,25 +264,9 @@ class Aerosol:
                 eps2,
                 tau_pred_min2,
                 tau_pred_max2,
-            ) = self.ahmader(
-                sensorID,
-                wave,
-                nwave,
-                iwnir_s,
-                iwnir_l,
-                self.nsd,
-                mindx2,
-                geom,
-                wv,
-                rhoa,
-            )
+            ) = self.ahmader(self.nsd, mindx2, wv, rhoa, solz, senz, phi)
 
             taua2, tsol2, tsen2 = self.diff_tran(
-                sensorID,
-                wave,
-                nwave,
-                iwnir_l,
-                geom,
                 wv,
                 pr,
                 taur,
@@ -281,6 +276,9 @@ class Aerosol:
                 rhoa2,
                 tau_pred_min2,
                 tau_pred_max2,
+                solz,
+                senz,
+                phi,
             )
 
             rhoa = (1.0 - wt) * rhoa1 + wt * rhoa2
@@ -308,9 +306,7 @@ class Aerosol:
             tsen,
         )
 
-    def ahmad_atm_corr(
-        self, sensorID, wave, nwave, iwnir_s, iwnir_l, nmodels, mindx, geom, wv, rhoa
-    ):
+    def ahmad_atm_corr(self, nmodels, mindx, wv, rhoa, solz, senz, phi):
 
         tau_iwnir_l = np.zeros(nmodels)
         lg_tau_iwnir_s = np.zeros(nmodels)
@@ -320,17 +316,17 @@ class Aerosol:
         eps_pred = np.zeros(nmodels)
 
         # compute the observed epsilon
-        eps_obs = rhoa[iwnir_s] / rhoa[iwnir_l]
+        eps_obs = rhoa[self.nir_s] / rhoa[self.nir_l]
 
         for im in range(nmodels):
             modl = mindx[im]
             # compute AOT at longest aerosol wavelength (iwnir_l)
-            ac, bc, cc, dc, ec = self.ms_eps_coef(modl, iwnir_l, wave, geom)
+            ac, bc, cc, dc, ec = self.ms_eps_coef(modl, solz, senz, phi)
 
-            iwtab_l = iwnir_l
-            iwtab_s = iwnir_s
+            iwtab_l = self.nir_l
+            iwtab_s = self.nir_s
 
-            ax = ac[iwtab_l] - np.log(rhoa[iwnir_l])
+            ax = ac[iwtab_l] - np.log(rhoa[self.nir_l])
             bx = bc[iwtab_l]
             cx = cc[iwtab_l]
             fx = bx * bx - 4.0 * ax * cx
@@ -354,7 +350,7 @@ class Aerosol:
             rho_iwnir_s_pred[im] = np.exp(lg_rho_iwnir_s_pred[im])
 
             # compute model epsilon
-            eps_pred[im] = rho_iwnir_s_pred[im] / rhoa[iwnir_l]
+            eps_pred[im] = rho_iwnir_s_pred[im] / rhoa[self.nir_l]
 
         epsnir = eps_obs
         im1, im2, mwt = self.model_select_ahmad(nmodels, mindx, eps_pred, eps_obs)
@@ -364,11 +360,12 @@ class Aerosol:
         modrat = mwt
         # compute tau_pred and rho_predicted
         tau_pred_min, rho_pred_min = self.comp_rhoa_ms_eps(
-            nwave, wave, geom, tau_iwnir_l[im1], modmin, iwnir_l
+            tau_iwnir_l[im1], modmin, solz, senz, phi
         )
         tau_pred_max, rho_pred_max = self.comp_rhoa_ms_eps(
-            nwave, wave, geom, tau_iwnir_l[im2], modmax, iwnir_l
+            tau_iwnir_l[im2], modmax, solz, senz, phi
         )
+
         # compute weighted tau_aer and rho_aer
         tau_aer = (1.0 - mwt) * tau_pred_min + mwt * tau_pred_max
         rho_aer = (1.0 - mwt) * rho_pred_min + mwt * rho_pred_max
@@ -386,15 +383,13 @@ class Aerosol:
             rho_aer,
         )
 
-    def comp_rhoa_ms_eps(
-        self, nwave: np.int32, wave, geom: Geom, tau_iwnir_l, modl, iwnir_l
-    ):
-        ac, bc, cc, dc, ec = self.ms_eps_coef(modl, iwnir_l, wave, geom)
+    def comp_rhoa_ms_eps(self, tau_iwnir_l, modl, solz, senz, phi):
+        ac, bc, cc, dc, ec = self.ms_eps_coef(modl, solz, senz, phi)
 
         # get the extinction coefficients and compute AOT at all wavelengths
 
         ext_modl = self.aertab.model[modl].extc
-        tau_pred = (ext_modl / ext_modl[iwnir_l]) * tau_iwnir_l
+        tau_pred = (ext_modl / ext_modl[self.nir_l]) * tau_iwnir_l
         lg_tau_pred = np.log(tau_pred)
 
         # compute rho_pred
@@ -416,28 +411,29 @@ class Aerosol:
         epsilonT = sorted(epsilonT, key=lambda x: x.eps_obs)
 
         # find bounding epsilon indices in table
-        for im in range(self.nmodels):
+        for im in range(nmodels):
             if eps_obs < epsilonT[im].eps_obs:
-                result_im = im
                 break
-        im1 = max(min(result_im - 1, self.nmodels - 1), 0)
-        im2 = max(min(result_im, self.nmodels - 1), 0)
+            else:
+                im = nmodels
+        im1 = max(min(im - 1, nmodels - 1), 0)
+        im2 = max(min(im, nmodels - 1), 0)
 
         # convert table indices to model indices of the input order
         modmin = epsilonT[im1].modnum
         modmax = epsilonT[im2].modnum
 
         # compute model weighting
-        modrat = (eps_obs - epsilonT[im1].eps_obs) / (
-            epsilonT[im2].eps_obs - epsilonT[im1].eps_obs
-        )
-
         if modmin == modmax:
             modrat = 1
+        else:
+            modrat = (eps_obs - epsilonT[im1].eps_obs) / (
+                epsilonT[im2].eps_obs - epsilonT[im1].eps_obs
+            )
 
         return modmin, modmax, modrat
 
-    def ms_eps_coef(self, modnum, iwnir_l, wave, geom: Geom):
+    def ms_eps_coef(self, modnum, solz_in, senz_in, phi_in):
         lut_wave = self.aertab.wave
         lut_solz = self.aertab.solz
         lut_phi = self.aertab.phi
@@ -465,24 +461,19 @@ class Aerosol:
             (lut_wave, lut_solz, lut_phi, lut_senz), ems_all
         )
 
-        solz = np.full_like(wave, geom.solz, np.float64)
-        phi = np.full_like(wave, geom.phi, np.float64)
+        solz = np.full_like(self.wave, solz_in, np.float64)
+        phi = np.full_like(self.wave, phi_in, np.float64)
         phi = np.abs(phi)
-        senz = np.full_like(wave, geom.senz, np.float64)
-        ac = ams_all_func(np.array([wave, solz, phi, senz]).transpose())
-        bc = bms_all_func(np.array([wave, solz, phi, senz]).transpose())
-        cc = cms_all_func(np.array([wave, solz, phi, senz]).transpose())
-        dc = dms_all_func(np.array([wave, solz, phi, senz]).transpose())
-        ec = ems_all_func(np.array([wave, solz, phi, senz]).transpose())
+        senz = np.full_like(self.wave, senz_in, np.float64)
+        ac = ams_all_func(np.array([self.wave, solz, phi, senz]).transpose())
+        bc = bms_all_func(np.array([self.wave, solz, phi, senz]).transpose())
+        cc = cms_all_func(np.array([self.wave, solz, phi, senz]).transpose())
+        dc = dms_all_func(np.array([self.wave, solz, phi, senz]).transpose())
+        ec = ems_all_func(np.array([self.wave, solz, phi, senz]).transpose())
         return ac, bc, cc, dc, ec
 
     def diff_tran(
         self,
-        sensorID,
-        wave,
-        nwave,
-        iwnir_l,
-        geom,
         wv,
         pr,
         taur,
@@ -492,27 +483,29 @@ class Aerosol:
         rhoa,
         tauamin,
         tauamax,
+        solz,
+        senz,
+        phi,
     ):
+        csolz = np.cos(np.deg2rad(solz))
+        csenz = np.cos(np.deg2rad(senz))
+
         # get diff trans sun to ground and ground to sensor, per band for each model
-        tsolmin, tsenmin = self.model_transmittance(
-            modmin, wave, nwave, geom.solz, geom.senz, tauamin
-        )
-        tsolmax, tsenmax = self.model_transmittance(
-            modmax, wave, nwave, geom.solz, geom.senz, tauamax
-        )
+        tsolmin, tsenmin = self.model_transmittance(modmin, solz, senz, tauamin)
+        tsolmax, tsenmax = self.model_transmittance(modmax, solz, senz, tauamax)
         tsol = tsolmin * (1.0 - modrat) + tsolmax * modrat
-        tmp_pressure_diff = np.exp(-0.5 * taur / geom.csolz * (pr / p0 - 1))
+        tmp_pressure_diff = np.exp(-0.5 * taur / csolz * (pr / p0 - 1))
         tsol = tsol * tmp_pressure_diff
 
         tsen = tsenmin * (1.0 - modrat) + tsenmax * modrat
-        tmp_pressure_diff = np.exp(-0.5 * taur / geom.csenz * (pr / p0 - 1))
+        tmp_pressure_diff = np.exp(-0.5 * taur / csenz * (pr / p0 - 1))
         tsen = tsen * tmp_pressure_diff
 
         taua = tauamin * (1 - modrat) + tauamax * modrat
 
         return taua, tsol, tsen
 
-    def model_transmittance(self, modnum, wave, nwave, solz, senz, taua):
+    def model_transmittance(self, modnum, solz, senz, taua):
         dtran_theta = self.aertab.dtran_theta
         dtran_a0 = self.aertab.model[modnum].dtran_a0
         dtran_b0 = self.aertab.model[modnum].dtran_b0
